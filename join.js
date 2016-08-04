009e308aaa5abc59750d124e6f778d2f1187eef5
Dylan & Madeleine & Mandy Baker <pair+dylanhul.madeleine-rashbrooke.mandy-kate@devbootcamp.com>
2016-08-04 15:44:30 +1200

    initial table join for posts page

---
+++ join.js
@@ -0,0 +1,14 @@
+var config = require('./knexfile').development
+var knex = require('knex')(config)
+
+
+knex.from ('posts')
+  .innerJoin('profiles', 'profiles.post_id', 'posts.author_id' )
+  .then (function (data) {
+    console.log("I got the tables to join!", data)
+    })
+  .catch(logError)
+
+  function logError (err) {
+    console.log('Bother: ', err)
+  }
